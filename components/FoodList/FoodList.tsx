82977a5819a11a0efd14825f7e8fb02e112cd7c7
matheiu13 <matheiuamiel@gmail.com>
2024-04-04 16:41:44 +0800

    added alt text to images

--- components/FoodList/FoodList.tsx
+++ components/FoodList/FoodList.tsx
@@ -7,7 +7,6 @@ import {
   Flex,
   Grid,
   Image,
-  MantineProvider,
   Text,
   TextInput,
   UnstyledButton,
@@ -30,7 +29,7 @@ export function FoodList() {
     filterData(value);
   };
   const filterData = (query: string) => {
-    const filterDataByInput = [...food].filter((item) =>
+    const filterDataByInput = [...filteredData].filter((item) =>
       item.name.toLowerCase().includes(query.toLowerCase())
     );
     setFilteredData(filterDataByInput);
@@ -43,17 +42,17 @@ export function FoodList() {
   const sortData = (value: boolean) => {
     let sortedData: InputForm[];
     if (value === true) {
-      sortedData = [...food].sort((a, b) => a.rating - b.rating);
+      sortedData = [...filteredData].sort((a, b) => a.rating - b.rating);
     } else {
-      sortedData = [...food].sort((a, b) => b.rating - a.rating);
+      sortedData = [...filteredData].sort((a, b) => b.rating - a.rating);
     }
     setFilteredData(sortedData);
   };
 
   // add new item by filling up a form
-  const addNewItem = (newItem: InputForm) => {
-    setFilteredData((updatedFood) => [...updatedFood, newItem]);
-    food.push(newItem);
+  const addNewItem = (newFoodItem: InputForm) => {
+    setFilteredData((previousFoodList) => [...previousFoodList, newFoodItem]);
+    // food.push(newItem);
     modals.closeAll();
     notifications.show({
       title: 'Success',
@@ -88,84 +87,97 @@ export function FoodList() {
 
   return (
     <>
-      <MantineProvider>
-        <ModalsProvider>
-          <Notifications position="bottom-right" zIndex={1000} limit={4} />
-          <Container p={0} pt="5vh">
-            <Flex w="full" gap={5} align="center">
-              <TextInput w="100%" onChange={handleChangeQuery} placeholder="Search for an item" />
+      <ModalsProvider>
+        <Notifications position="bottom-right" zIndex={1000} limit={4} />
+        <Container p={0} pt="5vh">
+          <Flex w="full" gap={5} align="center">
+            <TextInput w="100%" onChange={handleChangeQuery} placeholder="Search for an item" />
 
-              {toggler ? (
-                <Button onClick={handleSortClick}>
-                  Sort <IconSortDescending style={{ width: rem(42) }} stroke={2} />
-                </Button>
-              ) : (
-                <Button onClick={handleSortClick}>
-                  Sort <IconSortAscending style={{ width: rem(42) }} stroke={2} />
-                </Button>
-              )}
-              <Button onClick={openAddItem}>
-                Add Item{' '}
-                <IconSquareRoundedPlus style={{ width: rem(42), marginLeft: '5px' }} stroke={2.5} />
+            {toggler ? (
+              <Button onClick={handleSortClick}>
+                Sort Rating{' '}
+                <IconSortDescending style={{ width: rem(54), marginLeft: '5px' }} stroke={2} />
               </Button>
-            </Flex>
-            <br />
-            <Grid p={1}>
-              {filteredData.map((f, index) => (
-                <Grid.Col span={4} key={index}>
-                  <Card shadow="sm" padding="sm" radius="md" mih="22vh" withBorder>
-                    <Card.Section mb={10}>
-                      <Image h={200} w="full" src={f.imgURL} onClick={() => viewImage(f.imgURL)} />
-                    </Card.Section>
-                    <Text size="xl" fw={700}>
-                      {f.name}
-                    </Text>
-                    {openedCards[index] ? (
-                      <Box
-                        onClick={() => toggleCard(index)}
-                        style={{ cursor: 'pointer', display: 'none' }}
-                      >
-                        <Text mb={10} truncate>
-                          {f.desc}
-                        </Text>
-                      </Box>
-                    ) : (
-                      <>
-                        {f.desc.length >= 40 ? (
-                          <Box onClick={() => toggleCard(index)} style={{ cursor: 'pointer' }}>
-                            <Text size="sm" mb={10} truncate>
-                              {f.desc}
-                            </Text>
-                            <Text size="sm" mb={10}>
-                              <u>view more</u>
-                            </Text>
-                          </Box>
-                        ) : (
-                          <Box>
-                            <Text size="sm" mb={10} truncate>
-                              {f.desc}
-                            </Text>
-                          </Box>
-                        )}
-                      </>
-                    )}
-
-                    <Collapse in={openedCards[index]}>
-                      <Text size="sm" mb={10}>
+            ) : (
+              <Button onClick={handleSortClick}>
+                Sort Rating{' '}
+                <IconSortAscending style={{ width: rem(54), marginLeft: '5px' }} stroke={2} />
+              </Button>
+            )}
+            <Button onClick={openAddItem}>
+              Add Item{' '}
+              <IconSquareRoundedPlus style={{ width: rem(52), marginLeft: '5px' }} stroke={2.5} />
+            </Button>
+          </Flex>
+          <br />
+          <Grid p={1}>
+            {filteredData.map((f, index) => (
+              <Grid.Col span={4} key={index}>
+                <Card shadow="sm" padding="sm" radius="md" mih="60vh" withBorder>
+                  <Card.Section mb={10}>
+                    <Image
+                      h={200}
+                      w="full"
+                      src={f.imgURL}
+                      onClick={() => viewImage(f.imgURL)}
+                      alt={f.name || 'Food'}
+                    />
+                  </Card.Section>
+                  <Text size="xl" fw={700}>
+                    {f.name}
+                  </Text>
+                  {openedCards[index] ? (
+                    <Box
+                      onClick={() => toggleCard(index)}
+                      style={{ cursor: 'pointer', display: 'none' }}
+                    >
+                      <Text mb={10} truncate>
                         {f.desc}
                       </Text>
-                      <UnstyledButton onClick={() => toggleCard(index)}>
+                    </Box>
+                  ) : (
+                    <>
+                      {f.desc.length >= 40 ? (
+                        <Box
+                          onClick={() => toggleCard(index)}
+                          style={{ cursor: 'pointer', display: 'flex', flexWrap: 'wrap' }}
+                        >
+                          <Text size="sm" mb={10} truncate>
+                            {f.desc}
+                          </Text>
+                          <Text size="sm" mb={10} style={{ marginLeft: 'auto' }}>
+                            <u>view more</u>
+                          </Text>
+                        </Box>
+                      ) : (
+                        <Box>
+                          <Text size="sm" mb={10} truncate>
+                            {f.desc}
+                          </Text>
+                        </Box>
+                      )}
+                    </>
+                  )}
+
+                  <Collapse in={openedCards[index]}>
+                    <Text size="sm" mb={10}>
+                      {f.desc}
+
+                      <UnstyledButton
+                        onClick={() => toggleCard(index)}
+                        style={{ marginLeft: '0.5rem' }}
+                      >
                         <u>view less</u>
                       </UnstyledButton>
-                    </Collapse>
-                    <Text>Rating: {f.rating}/5</Text>
-                  </Card>
-                </Grid.Col>
-              ))}
-            </Grid>
-          </Container>
-        </ModalsProvider>
-      </MantineProvider>
+                    </Text>
+                  </Collapse>
+                  <Text mt="auto">Rating: {f.rating}/5</Text>
+                </Card>
+              </Grid.Col>
+            ))}
+          </Grid>
+        </Container>
+      </ModalsProvider>
     </>
   );
 }
