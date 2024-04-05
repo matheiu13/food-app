c1ba590a9ce8cece6e5f44a3d3b9dba7ed457c60
Matheiu Perdido <92298544+matheiu13@users.noreply.github.com>
2024-04-05 11:23:15 +0800

    Remove yarn (#8)
    * add card feature
    
    * install dependencies
    
    * delete food-list.tsx
    
    * reinstall dependency
    
    * uninstall yarn

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import { Welcome } from '../components/Welcome/Welcome';
 import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
+import { FoodList } from '../components/FoodList/FoodList';
 
 export default function HomePage() {
   return (
     <>
       <Welcome />
       <ColorSchemeToggle />
+      <FoodList />
     </>
   );
 }
