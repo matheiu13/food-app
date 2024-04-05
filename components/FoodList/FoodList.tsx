9f50cd83abeb0958abd0562736d0a7fbe7102fe7
matheiu13 <matheiuamiel@gmail.com>
2024-04-05 13:00:33 +0800

    add sort by rating of food

--- components/FoodList/FoodList.tsx
+++ components/FoodList/FoodList.tsx
@@ -1,9 +1,11 @@
-import { Card, Container, Flex, Grid, TextInput, Image, Text } from '@mantine/core';
+import { Card, Container, Flex, Grid, TextInput, Image, Text, Button, rem } from '@mantine/core';
 import { ChangeEvent, useState } from 'react';
+import { IconSortAscending, IconSortDescending } from '@tabler/icons-react';
 import { food } from '../../constants/dummyDB.js';
 
 export function FoodList() {
   const [foodData, setFoodData] = useState(food);
+  const [isAscending, setIsAscending] = useState(false);
   const handleChangeQuery = (e: ChangeEvent<HTMLInputElement>): void => {
     const { value } = e.target;
     filterFoodData(value);
@@ -14,11 +16,33 @@ export function FoodList() {
     );
     setFoodData(filterDataByInput);
   };
+  const handleSortClick = () => {
+    sortData(isAscending);
+    setIsAscending(!isAscending);
+  };
+  const sortData = (value: boolean) => {
+    if (value === true) {
+      const sortedData = [...food].sort((a, b) => a.rating - b.rating);
+      setFoodData(sortedData);
+    } else {
+      const sortedData = [...food].sort((a, b) => b.rating - a.rating);
+      setFoodData(sortedData);
+    }
+  };
   return (
     <>
       <Container p={0} pt="5vh">
         <Flex w="full" gap={5} align="center" mb={10}>
           <TextInput w="100%" placeholder="Search for an item" onChange={handleChangeQuery} />
+          {isAscending ? (
+            <Button onClick={handleSortClick}>
+              Sort Rating <IconSortDescending style={{ width: rem(42) }} stroke={2} />
+            </Button>
+          ) : (
+            <Button onClick={handleSortClick}>
+              Sort Rating <IconSortAscending style={{ width: rem(42) }} stroke={2} />
+            </Button>
+          )}
         </Flex>
         <Grid p={1}>
           {foodData.map((f, index) => (
